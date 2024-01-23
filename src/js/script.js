cd800affc6efe9f001a5512abf70b0a218bc7a27
silverskg <chokichoki_gm@i.softbank.jp>
2024-01-23 20:27:46 +0900

    pc_スタイル作成

--- src/js/script.js
+++ src/js/script.js
@@ -1,14 +1,21 @@
-jQuery(function ($) { // $はjQueryを表す
-    // この中にコードを書く
-
-    // 例：クラスがc-btnのcolorを黒色にする
-    // $(".c-btn").css("color","black")
-
-    // 例：idがbuttonのテキストを変更
-    // $("#button").text("ボタンのテキストを変更");
-
-    // 例：クラスがc-btnをクリックした場合、コンソールログを出力
-    // $(".c-btn").click(function() {
-    //     console.log("ボタンがクリックされました！");
-    // });
+jQuery(function ($) {
+    var service_swiper = new Swiper(".js-service-swiper", {
+        loop: true,
+        speed: 2000,
+        slidesPerView: 3,
+        centeredSlides: true,
+        autoplay: {
+            delay: 2000,
+            disableOnInteraction: false,
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 5,
+            }
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
 });
