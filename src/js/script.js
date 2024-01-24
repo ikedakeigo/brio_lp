2090278167f9055598af9b231e47358093c12f61
silverskg <chokichoki_gm@i.softbank.jp>
2024-01-24 19:46:44 +0900

    pc_スライダー

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,8 @@
 jQuery(function ($) {
-    var service_swiper = new Swiper(".js-service-swiper", {
+    const service_swiper = new Swiper(".js-service-swiper", {
         loop: true,
-        speed: 2000,
-        slidesPerView: 3,
+        speed: 1000,
+        slidesPerView: 1,
         centeredSlides: true,
         autoplay: {
             delay: 2000,
@@ -10,12 +10,32 @@ jQuery(function ($) {
         },
         breakpoints: {
             768: {
-                slidesPerView: 5,
+                slidesPerView: 3,
             }
         },
         pagination: {
             el: ".swiper-pagination",
             clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next', //「次へボタン」要素の指定
+            prevEl: '.swiper-button-prev', //「前へボタン」要素の指定
+        },
+    });
+});
+
+
+jQuery(function ($) {
+    const mv_swiper = new Swiper(".js-mv-swiper", {
+        loop: true,
+        speed: 2000,
+        effect: "fade",
+        fadeEffect: {
+            crossFade: true,
+        },
+        autoplay: {
+            delay: 4000,
+            disableOnInteraction: false,
         },
     });
 });
